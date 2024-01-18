d293d130a1920cdf6717427aa0f8ae531fadbfac
ArtiTwink <150339206+ArtiTwink@users.noreply.github.com>
2024-01-18 13:21:11 +0300

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{ 
+    //поля
+
+    //HP
+    public int heatl = 10;
+    public int maxHeatl = 10;
+    //Урон
+    public int damage = 2;
+
+    public AudioSource audioSource;
+
+    public AudioClip damageSound;
+
+
+    //Количество монет
+    public int coins;
+    //Снаряд
+    public GameObject fireballPrefab;
+    //Появление с.
+    public Transform attackPoint;
+
+    //Кадр
+    private void Update()
+    {
+        //ЛКМ
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Копия
+            //Поворот
+            Instantiate(fireballPrefab, attackPoint.position, attackPoint.rotation);
+        }
+    }
+
+    //Уменьшение HP
+    public void TakeDamage(int damage)
+    {
+        heatl -= damage;
+        if (heatl > 0)
+        {
+            audioSource.PlayOneShot(damageSound);
+            print("Здоровье игрока: " + heatl);
+
+        }
+        else
+        {
+            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(sceneIndex);
+        }
+    }
+    public void CollectCoins()
+    {
+        coins += 1;
+        print("Coins" + coins);
+    }
+}
+     
